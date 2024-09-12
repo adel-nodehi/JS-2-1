cd47c733923994dec0c6c37cea4a6d8db7722848
Adel <adelnmo2003@gmail.com>
2024-09-12 06:09:53 +0330

    array and array methods

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ const age = 17;
 const string = `you are ${age > 18 ? "adult" : "young"}`;
 */
 
+/*
 function printHello() {
   console.log("Hello");
 }
@@ -59,3 +60,89 @@ function displayInfo(name, age) {
 }
 
 // displayInfo("adel", 12);
+*/
+/*
+console.log(calculateAge(2006));
+
+function calculateAge(birthYear) {
+  return 2024 - birthYear;
+}
+
+//
+
+const calculateAge2 = function (bithYear) {
+  return 2024 - bithYear;
+};
+
+console.log(calculateAge2(2006));
+
+
+const calculateAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const calculateAge3 = (birthYear) => {
+  return 2024 - birthYear;
+};
+
+console.log(calculateAge3(2004));
+*/
+
+// const user1 = "shiva";
+// const user2 = "hossein";
+// const user3 = "sina";
+
+const users = [
+  "shiva",
+  "negin",
+  "hossein",
+  "cena",
+  "ali",
+  "nerges",
+  "shayan",
+  "saeed",
+  "maryam",
+];
+
+if (users.includes("shiva")) {
+  console.log(`shiva exist and the index is ${users.indexOf("shiva")}`);
+} else {
+  console.log(`shiva does not exist`);
+}
+// negin exist and the index is 1
+
+// negin does not exist
+
+// console.log(users);
+
+// users.shift();
+// users.shift();
+// users.pop();
+// users.push(users.length);
+// users.unshift(users.length);
+
+// console.log(users);
+
+/*
+[
+    6,
+    "cena",
+    "ali",
+    "nerges",
+    "shayan",
+    "negin",
+    5,
+];
+*/
+
+// console.log(users);
+
+// add an element
+// console.log(users.push(5));
+// users.unshift(true);
+
+// delete an element
+// console.log(users.shift());
+// users.shift();
+
+// console.log(users);
